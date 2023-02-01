b765cd0ca8aeb7dedb786687f1b7d4e1818a4254
Artem <Artem@DESKTOP-A585VQ9>
2023-02-01 22:02:47 +0200

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassForIntegerArray;
+internal class IntegerArray
+{
+    private List<int> Array;
+
+    public IntegerArray()
+    {
+        Array = new List<int>();
+    }
+
+    public IntegerArray(int[] Temporary)
+    {
+        Array = new List<int>();
+        for (int i = 0; i < Temporary.Length; i++)
+            Array.Add(Temporary[i]);
+    }
+
+    public IntegerArray(string Temporary)
+    {
+        Array = new List<int>();
+
+        for (int i = 0; i < Temporary.Length; i++)
+        {
+            int Temp = 0;
+            for (int j = i; j < Temporary.Length; j++)
+            {
+                if (Temporary[i] >= '0' && Temporary[i] <= '9')
+                {
+                    Temp = Temp * 10 + Convert.ToInt32(Temporary[i]);
+                    if (j == Temporary.Length) Array.Add(Temp);
+                }
+                else
+                {
+                    Array.Add(Temp);
+                    i = j;
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Lesson1.cs
@@ -0,0 +1,46 @@
+﻿using ClassForIntegerArray;
+using System.Xml.Linq;
+
+List<IntegerArray> Массивы = new List<IntegerArray>();
+
+while (true)
+{
+    Console.WriteLine("Выберите действие:");
+    Console.WriteLine("1. Создать пустой массив");
+    Console.WriteLine("2. Добавить сотрудника(Подробный вариант)");
+    Console.Write("\nВыбор: ");
+
+    string inputString = Console.ReadLine();
+    try
+    {
+        string Выбор = inputString;
+
+        switch (Выбор)
+        {
+            case "1":
+                {
+                    // Сотрудник Новичек = new Сотрудник(familia, name, null, age, null, null);
+                    IntegerArray.new();
+                    break;
+                }
+
+            case "2":
+                {
+
+                    break;
+                }
+
+            default:
+                {
+                    Console.WriteLine("Такой вариант не предусмотрен.");
+                    break;
+                }
+        }
+        Console.WriteLine("Нажмите любую клавишу для продолжения...");
+        Console.ReadLine();
+    }
+    finally
+    {
+        Console.Clear();
+    }
+}
